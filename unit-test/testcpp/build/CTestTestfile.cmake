98f9d0a50e21cce2871e6256ecf2e4eff6a57c4e
som3a13 <134228578+som3a13@users.noreply.github.com>
2024-06-04 01:56:10 +0300

    UPDATE

---
+++ unit-test/testcpp/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/som3a133/Desktop/EmbeddedLinux/NTI/play/ELa/s5/unit test/testcpp
+# Build directory: /home/som3a133/Desktop/EmbeddedLinux/NTI/play/ELa/s5/unit test/testcpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[hx]=] "/home/som3a133/Desktop/EmbeddedLinux/NTI/play/ELa/s5/unit test/testcpp/build/hx")
+set_tests_properties([=[hx]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/som3a133/Desktop/EmbeddedLinux/NTI/play/ELa/s5/unit test/testcpp/CMakeLists.txt;14;add_test;/home/som3a133/Desktop/EmbeddedLinux/NTI/play/ELa/s5/unit test/testcpp/CMakeLists.txt;0;")
